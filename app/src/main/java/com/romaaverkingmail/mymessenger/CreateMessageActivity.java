471dd95c9b318f909d221896fef8a4ae4377e61a
Roman <romaaverkin@gmail.com>
2018-12-17 16:29:38 +0700

    In works.

--- app/src/main/java/com/romaaverkingmail/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/romaaverkingmail/mymessenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -14,7 +15,10 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage(View view) {
+        EditText messageView = findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
